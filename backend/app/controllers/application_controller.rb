5df305c70b212d07741fdab181a98a866c082dad
Shimon-Kuwahara <tukubamj@icloud.com>
2024-11-05 10:59:31 +0900

    deiviseでセッションが使われないことへのエラーの対応

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        include DeviseHackFakeSession # Rails7より、APIモードのようにセッションを使用しない場合にセッションへのアクセスがあるとエラーを発生させる機構が備わったことによるエラーです。deviseでは内部でセッションにアクセスしている箇所があるので当該エラーが発生する模様です。
+
+        
 
         before_action do
                 I18n.locale = :ja
