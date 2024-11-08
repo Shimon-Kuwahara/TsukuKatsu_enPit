4c63b09b3ba34413dd2973f6cfa03bd7a3e46c06
Shimon-Kuwahara <tukubamj@icloud.com>
2024-11-08 11:48:12 +0900

    companyモデルの作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  resources :companies
+  resources :recruitments
+
+  get 'users', to: 'users#index'
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
-  resources :recruitments
-  get 'users', to: 'users#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
