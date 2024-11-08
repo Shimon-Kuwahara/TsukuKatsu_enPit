37ca1ce9b6ac006246f096eeb064fc2dfdb80505
Shimon-Kuwahara <tukubamj@icloud.com>
2024-11-08 11:08:18 +0900

    ホーム画面の調整

--- frontend-user/src/components/layouts/AuthButton.tsx
+++ frontend-user/src/components/layouts/AuthButton.tsx
@@ -1,40 +1,40 @@
 "use client";
-import React from 'react'
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import { doesCookieExist } from '../../utils/cookieUtils';
+import React from "react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { doesCookieExist } from "../../utils/cookieUtils";
 
 export default function AuthButton() {
   const [isUidPresent, setIsUidPresent] = useState(false);
   useEffect(() => {
-    setIsUidPresent(doesCookieExist('uid'));
+    setIsUidPresent(doesCookieExist("uid"));
   }, []);
 
   return (
-    <div className="flex items-center space-x-2">
-        {isUidPresent ? (
-          <Link
+    <div>
+      {isUidPresent ? (
+        <Link
           href="/"
           className="border border-main-col hover:bg-gray-100 py-1 px-2 rounded"
+        >
+          ログイン済
+        </Link>
+      ) : (
+        <>
+          <Link
+            href="/sign_in"
+            className="bg-main-col hover:bg-purple-700 text-white py-1 px-2 rounded"
           >
-            ログアウト
+            ログイン
           </Link>
-        ) : (
-          <>
-            <Link
-              href="/sign_in"
-              className="bg-main-col hover:bg-purple-700 text-white py-1 px-2 rounded"
-            >
-              ログイン
-            </Link>
-            <Link
-              href="/sign_up"
-              className="border border-main-col hover:bg-gray-100 py-1 px-2 rounded"
-            >
-              会員登録
-            </Link>
-          </>
-        )}
-      </div>
-  )
+          <Link
+            href="/sign_up"
+            className="border border-main-col hover:bg-gray-100 py-1 px-2 rounded"
+          >
+            会員登録
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
