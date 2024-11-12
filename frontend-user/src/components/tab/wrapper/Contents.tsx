4bf7b82151192b5aa1164d01c42e412f4b6e926e
OGATA Hiromu <hiromukkun@gmail.com>
2024-11-12 17:09:55 +0900

    add:タブ実装

---
+++ frontend-user/src/components/tab/wrapper/Contents.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import Menu from "./Menu";
+import { TabContext } from "@/app/companies/[id]/page";
+import Blog from "../contents/Blog";
+import Recruitment from "../contents/Recruitment";
+import Review from "../contents/Review";
+
+export default function Contents() {
+	const { tabIndex } = useContext(TabContext);
+	return (
+		<>
+			<Menu />
+			{tabIndex === 0 && <Recruitment />}
+			{tabIndex === 1 && <Review />}
+			{tabIndex === 2 && <Blog />}
+		</>
+	);
+}
