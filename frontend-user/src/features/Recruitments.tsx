4b1a0048eb16082d06b43e01b68d85cc7870215a
桑原 士門 <130543301+Shimon-Kuwahara@users.noreply.github.com>
2024-11-13 14:21:17 +0900

    Revert "サーバーサイドでのデータフェッチの実装と設定"

--- frontend-user/src/features/Recruitments.tsx
+++ frontend-user/src/features/Recruitments.tsx
@@ -1,25 +1,40 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import RecruitmentCard from "@/components/elements/RecruitmentCard";
 import { Recruitment } from "@/types/recruitment";
 
-export default async function Recruitments() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}recruitments`,
-    { cache: "no-store" } // 必要に応じてキャッシュ設定
-  );
-  if (!response.ok) {
-    throw new Error("データの取得に失敗しました");
-  }
-  const recruitments: Recruitment[] = await response.json();
+export default function Recruitments() {
+  const [recruitments, setRecruitments] = useState<Recruitment[]>([]);
+
+  useEffect(() => {
+    const fetchRecruitments = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}recruitments`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result: Recruitment[] = await response.json();
+        setRecruitments(result);
+      } catch (error) {
+        console.error((error as Error).message);
+      }
+    };
+
+    fetchRecruitments();
+  }, []);
 
   return (
     <>
+      {/* <h1 className="text-xl font-bold p-4">求人はこちら</h1> */}
       <div className="flex flex-wrap justify-center">
         {recruitments.length > 0 ? (
           recruitments.map((recruitment) => (
             <RecruitmentCard key={recruitment.id} recruitment={recruitment} />
           ))
         ) : (
-          <p>求人が見つかりませんでした。</p>
+          <p>Loading recruitments...</p>
         )}
       </div>
     </>
