643950bac677182e4af655c13c7acf30864d828e
raimu <raijirou12@gmail.com>
2024-11-15 16:39:26 +0900

    企業ページからブログに飛べるようにした

--- frontend-user/src/components/tab/contents/Blog.tsx
+++ frontend-user/src/components/tab/contents/Blog.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Blog() {
-  return <>blog</>;
+  return (
+    <div className="text-center mt-4">
+      <Link href="/blog" className="btn btn-primary btn-lg">
+        桑原しもんさんのブログを見る
+      </Link>
+    </div>
+  );
 }
