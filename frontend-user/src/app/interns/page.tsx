14765196a721f13bb9b78222041871ccf02e5c1b
Shimon-Kuwahara <tukubamj@icloud.com>
2024-12-28 15:44:01 +0900

    ディレクトリ構成の一新

---
+++ frontend-user/src/app/interns/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>全取得</div>
+  )
+}
---
+++ frontend-user/src/features/pages/IndexInterns.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function IndexInterns() {
+  return (
+    <>
+      <div>インターン情報一覧を作る</div>
+      <div>もっと見る</div>
+    </>
+  );
+}
