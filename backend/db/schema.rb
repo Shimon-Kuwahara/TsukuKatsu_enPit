2c54a5686a99889df976fef97dee7592df259561
Shimon-Kuwahara <tukubamj@icloud.com>
2024-12-28 11:16:05 +0900

    internモデルに求人モデルの外部キーの追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_28_020836) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_28_021304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -213,6 +213,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_28_020836) do
   add_foreign_key "chat_rooms", "companies"
   add_foreign_key "chat_rooms", "recruitments"
   add_foreign_key "chat_rooms", "users"
+  add_foreign_key "interns", "recruitments"
   add_foreign_key "messages", "chat_rooms"
   add_foreign_key "recruitments", "companies"
   add_foreign_key "recruitments", "users"
