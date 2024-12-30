ae47d30f2fa381b185dfa76d8949b76ad7ae75d1
Shirasawa <yrry.nrm@gmail.com>
2024-12-31 05:23:04 +0900

    fix: ホームページとintern一覧のtitile変更

--- frontend-user/src/app/interns/page.tsx
+++ frontend-user/src/app/interns/page.tsx
@@ -1,6 +1,11 @@
 import Interns from '@/features/interns/pages/Interns'
 import React from 'react'
 
+export const metadata = {
+  title: "長期インターン一覧 | Tsukuba Intern",
+  description: "Tsukuba Intern インターン一覧",
+};
+
 export default function page() {
   return (
     <Interns />
