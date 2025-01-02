7104df4ddb67f185d79df9fbceb45f8ad370c68d
Shimon-Kuwahara <tukubamj@icloud.com>
2025-01-02 18:49:04 +0900

    細かいUIの修正(テストデータが短絡的だからこの後改善)

--- frontend-user/src/features/interns/id/components/InternDetails.tsx
+++ frontend-user/src/features/interns/id/components/InternDetails.tsx
@@ -7,71 +7,76 @@ type InternDetailsProps = {
 
 export default function InternDetails({ intern }: InternDetailsProps) {
   return (
-    <div className="max-w-3xl mx-auto">
-      <div className="p-4">
-        <div className="flex items-start gap-4 mb-6">
-          <div className="relative w-24 h-24 rounded-lg overflow-hidden flex-shrink-0">
-            <Image
-              src={"/images/default_profile.jpg"}
-              alt="Profile"
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold text-main-col">
-              {intern.department}
-            </h1>
-            <h2 className="text-xl mb-1">{intern.grade}</h2>
-            <p className="text-lg">{intern.nickname}さん</p>
-          </div>
+    <div className="max-w-3xl mx-auto p-4 space-y-6">
+      <div className="flex items-start gap-4">
+        <div className="relative w-24 h-24 flex-shrink-0 overflow-hidden rounded-lg">
+          <Image
+            src="/default_profile.png"
+            alt="Profile"
+            fill
+            className="object-cover"
+          />
         </div>
-        <div className="space-y-4">
-          <section className="bg-gray-200 rounded-lg p-4">
-            <h3 className="font-semibold mb-2">基本データ</h3>
-            <dl className="space-y-2">
-              <div>
-                <dt className="inline">学類: </dt>
-                <dd className="inline">
-                  {intern.department}
-                  {intern.grade}
-                </dd>
-              </div>
-              <div>
-                <dt className="inline">研究室: </dt>
-                <dd className="inline">{intern.labo}</dd>
-              </div>
-              <div>
-                <dt className="inline">サークル: </dt>
-                <dd className="inline">{intern.club}</dd>
-              </div>
-            </dl>
-          </section>
-
-          <section className="bg-gray-200 rounded-lg p-4">
-            <h3 className="font-semibold mb-2">実績</h3>
-            <ul className="list-disc list-inside">
-              {/* {intern.achievements.map((achievement, index) => (
-              <li key={index}>{achievement}</li>
-            ))} */}
-            </ul>
-          </section>
-
-          <section className="bg-gray-200 rounded-lg p-4">
-            <h3 className="font-semibold mb-2">インターン応募時の経験</h3>
-            <p>{intern.application_reason}</p>
-          </section>
+        <div>
+          <h1 className="text-2xl font-bold text-main-col">
+            {intern.department}
+          </h1>
+          <h2 className="text-xl mb-1">{intern.grade}</h2>
+          <p className="text-lg">{intern.nickname}さん</p>
         </div>
       </div>
-      <div className="p-4">
-        <header className="bg-purple-600 text-white p-4 rounded-t-lg flex justify-between items-center">
+
+      {/* 基本データや実績など */}
+      <div className="space-y-4">
+        <section className="bg-gray-200 rounded-lg p-4">
+          <h3 className="mb-2 font-semibold">基本データ</h3>
+          <dl className="space-y-2">
+            <div>
+              <dt className="inline">学類: </dt>
+              <dd className="inline">
+                {intern.department}
+                {intern.grade}
+              </dd>
+            </div>
+            <div>
+              <dt className="inline">研究室: </dt>
+              <dd className="inline">{intern.labo}</dd>
+            </div>
+            <div>
+              <dt className="inline">サークル: </dt>
+              <dd className="inline">{intern.club}</dd>
+            </div>
+          </dl>
+        </section>
+
+        <section className="bg-gray-200 rounded-lg p-4">
+          <h3 className="mb-2 font-semibold">実績</h3>
+          <ul className="list-disc list-inside">
+            {/* {intern.achievements.map((achievement, index) => (
+              <li key={index}>{achievement}</li>
+            ))} */}
+          </ul>
+        </section>
+
+        <section className="bg-gray-200 rounded-lg p-4">
+          <h3 className="mb-2 font-semibold">インターン応募時の経験</h3>
+          <p>{intern.application_reason}</p>
+        </section>
+      </div>
+
+      {/* インターン詳細 */}
+      <div>
+        <header className="flex items-center justify-between bg-main-col p-4 text-white rounded-t-lg">
           <h1 className="text-xl font-bold">
             {intern.nickname}さんのインターン体験記
           </h1>
         </header>
-        <span className="text-sm">更新日: {intern.updated_at}</span>
-
-        <div className="space-y-6 bg-white p-6 rounded-b-lg shadow">
+        {/* 更新日表示 */}
+        <span className="text-sm block p-4 pt-2 pb-0">
+          更新日: {intern.updated_at}
+        </span>
+        {/* コンテンツ本体 */}
+        <div className="space-y-16 p-4 rounded-b-lg">
           <section>
             <h2 className="text-purple-600 text-lg font-bold mb-2">
               インターン内容
@@ -88,9 +93,7 @@ export default function InternDetails({ intern }: InternDetailsProps) {
           </section>
 
           <section>
-            <h2 className="text-purple-600 text-lg font-bold mb-2">
-              週勤務時間
-            </h2>
+            <h2 className="text-purple-600 text-lg font-bold mb-2">週勤務時間</h2>
             {/* {intern.weeklyHours.map((hours, index) => (
               <p key={index} className="text-gray-700">
                 {hours}
